6d61dfb2cdb32699528b8452d6bf9364aa5ebfea
weimingxuan <793270445@qq.com>
2017-06-03 14:37:56 +0800

    no message

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -2,9 +2,12 @@
 
 namespace app\api\service;
 
-use app\api\model\Product as ProductModel
+use app\api\model\Product as ProductModel;
+use app\api\model\UserAddress as UserAddressModel;
 
+use Exception;
 use app\lib\exception\OrderException;
+use app\lib\exception\UserException;
 
 class Order
 {
@@ -18,16 +21,89 @@ class Order
 
   public function place($uid,$oProducts){
     //$oProducts,$products做对比
-
     //$products从数据查出来
     $this->oProducts = $oProducts;
-    $this->products = getProductsByOrder($oProducts);
+    $this->products = $this->getProductsByOrder($oProducts);
     $this->uid = $uid;
+    $status = $this->getOrderStatus();
+    if (!$status['pass']) {
+      $status['order_id'] = -1;
+      return $status;
+    }
+    //开始创建订单
+    $ordersSnap = $this->snapOrder($status);
+    // createOrder($ordersSnap);
   }
+
+  private function createOrder($snap){
+    try {
+      $orderNo = $this->makeOrderNo();
+      $order = new \app\api\model\Order();
+      $order->user_id = $this->uid;
+      $order->order_no = $orderNo;
+      $order->total_price = $snap['orderPrice'];
+      $order->total_count = $snap['orderCount'];
+      $order->total_img = $snap['orderImg'];
+      $order->total_name = $snap['orderName'];
+      $order->total_address = $snap['orderAddress'];
+      $order->total_items = json_encode($snap['pStatus']);
+      $order->save();
+      $orderID = $order->id;
+      $create_time = $order->create_time;
+      foreach ($this->$oProducts as &$oProduct) {
+        $oProduct['order_id'] = $orderID;
+      }
+      $orderProduct = new \app\api\model\OrderProduct();
+      $orderProduct->saveAll($this->$oProducts);
+      return [
+        "order_no" => $orderNo,
+        "order_id" => $orderID,
+        "create_time" => $create_time
+      ];
+    } catch (Exception $e) {
+        throw $e;
+    }
+  }
+
+  //生成订单快照
+  private function snapOrder($status){
+    $snap = [
+      "orderPrice" => 0,
+      "totalCount" => 0,
+      "pStatus" => [],
+      "snapAddress" => null,
+      "snapName" => "",
+      "snapImg" => ""
+    ];
+    $snap['orderPrice'] = $status['orderPrice'];
+    $snap['totalCount'] = $status['totalCount'];
+    $snap['pStatus'] = $status['pStatusArray'];
+    $snap['snapAddress'] => json_encode($this->getUserAddress());
+    $snap['snapName'] => $this->products[0]['name'];
+    $snap['snapImg'] => $this->products[0]['main_img_url'];
+    if (count($this->products) > 1) {
+      $snap['snapName'] = $snap['snapName']."等";
+    }
+    return $snap;
+  }
+
+  private function getUserAddress(){
+    $userAddress = UserAddressModel::where("user_id","=",$this->uid)
+      ->find();
+    if (!$userAddress) {
+      throw new UserException([
+        "msg" => "用户收货地址不存在，下单失败",
+        "errorCode" => "60001",
+      ]);
+    }
+    return $userAddress->toArray();
+  }
+
   private function getOrderStatus(){
     $status = [
       'pass' => true,
       'orderPrice' => 0,
+      'totalCount' = > 0,
       'pStatusArray' => []
     ];
     foreach ($this->oProducts as $oProduct) {
@@ -40,6 +116,7 @@ class Order
         $status['pass'] = false;
       }
       $status['orderPrice'] += $pStatus['totalPrice'];
+      $status['totalCount'] += $pStatus['count'];
       array_push($status['pStatusArray'],$pStatus);
     }
     return $status;
@@ -84,5 +161,15 @@ class Order
       ->visible(["id","price","stock","name","main_img_url"])
       ->toArray();
     return $products;
+  }
+
+  public static function makeOrderNo()
+  {
+      $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+      $orderSn =
+          $yCode[intval(date('Y')) - 2017] . strtoupper(dechex(date('m'))) . date(
+              'd') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf(
+              '%02d', rand(0, 99));
+      return $orderSn;
   }
 }
