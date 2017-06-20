de34641c921d90e6dd86424c85b138d8ee5e53fc
weimingxuan <793270445@qq.com>
2017-06-20 14:57:30 +0800

    no message

---
+++ application/api/service/WxNotify.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\api\service;
+
+use app\api\model\Order as OrderModel;
+use app\api\model\Product as ProductModel;
+use app\api\service\Order as OrderService;
+
+
+use app\lib\enum\OrderStatusEnum;
+
+use think\Log;
+use think\Loader;
+Loader::import("WxPay.WxPay",EXTEND_PATH,".Api.php");
+
+class WxNotify extends \WxPayNotify
+{
+  public function NotifyProcess($data,&$msg){
+    if ($data['result_code'] == "SUCCESS") {
+      $orderNo = $data['out_trade_no'];
+      try {
+        $order = OrderModel::where("order_no","=",$orderNo)
+          ->find();
+        if ($order->status == 1) {
+          $service = new OrderService();
+          $stockStatus = $service->checkOrderStock($order->id);
+          if ($stockStatus['pass']) {
+            $this->updateOrderStatus($order->id, true);
+            $this->reduceStock();
+          }else{
+            $this->updateOrderStatus($order->id, false);
+          }
+        }
+        return true;
+      } catch (Exception $e) {
+        Log::error($e);
+        return false;
+      }
+    }else{
+      return true;
+    }
+  }
+
+  private function reduceStock($stockStatus){
+    foreach ($stockStatus['pStatusArray'] as $singlePStatus) {
+      ProductModel::where("id","=",$singlePStatus["id"])
+        ->setDec("stock",$singlePStatus['count']);
+    }
+  }
+
+  private function updateOrderStatus($orderID,$success){
+    $status = $success?OrderStatusEnum::PAID :
+      OrderStatusEnum::PAID_BUT_OUT_OF;
+    OrderModel::where("id","=",$orderID)
+      ->update(['status'] => $status);
+  }
+}
