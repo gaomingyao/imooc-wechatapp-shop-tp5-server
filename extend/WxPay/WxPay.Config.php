cd878450b4493c3f2635b0b2a199ef3eb13c132d
魏铭轩 <793270445@qq.com>
2017-10-20 11:35:32 +0800

    Update WxPay.Config.php

--- extend/WxPay/WxPay.Config.php
+++ extend/WxPay/WxPay.Config.php
@@ -22,10 +22,10 @@ class WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx19581cbe9b0d5e7c';
-	const MCHID = '1900009851';
-	const KEY = '8934e7d15453e97507ef794cf7b0519d';
-	const APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
+	const APPID = '';
+	const MCHID = '';
+	const KEY = '';
+	const APPSECRET = '';
 	
 	//=======【证书路径设置】=====================================
 	/**
