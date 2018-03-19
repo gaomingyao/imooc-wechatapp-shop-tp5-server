f344e227a002fc9451fb4ac2208b939cad162c0b
魏铭轩 <793270445@qq.com>
2018-03-19 22:00:22 +0800

    Update wx.php

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -2,7 +2,7 @@
 return [
   'app_id' => 'wx19581cbe9b0d5e7c',
   
-  'app_secret' => '101b81b03d85e5714bef7577140d3228',
+  'app_secret' => '',
 
   'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?'.
     'appid=%s&secret=%s&js_code=%s&grant_type=authorization_code',
